6db00f6f8e0ea86ac9aa4fa0b0bc0b24829d965a
anik-aryan <127028550+anik-aryan@users.noreply.github.com>
2025-07-31 22:21:45 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,3 +1,21 @@
-#Write your code here in python 
-n=int(input("enter a nuber"))
-print(n)
+
+marks = int(input())
+if(marks>=90 and marks <=100):
+    print("A")
+elif(marks>=75 and marks<90):
+    print("B")
+elif(marks>=60 and marks<75):
+    print("C")
+elif(marks>=40 and marks<60):
+    print("D")
+elif(marks<40 and marks>=0):
+    print("F")
+            
+else:
+    print("Enter a valid marks")
+            
+            
+            
+            
+      
+ 
